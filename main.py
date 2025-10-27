3c9e5658256aff637b6e8ab4979503486104f36b
Ali Qazipoor <qazipoor@gmail.com>
2025-10-28 00:03:54 +0300

    Added functionality

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
+while machine_is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}: ")
+
+    if choice == "off":
+        print("The machine is turned off.")
+        machine_is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+    elif choice == "profit":
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
