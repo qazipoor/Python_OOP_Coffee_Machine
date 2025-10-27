83d7bc9b525b7a41dd56a89de846e130107e33e3
Ali Qazipoor <qazipoor@gmail.com>
2025-10-28 00:03:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+machine_is_on = True
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
